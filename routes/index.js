1bef834f610afcc0ff3df3bb77f63284772d2a20
LeoChen_Mac <leochendesign@126.com>
2019-06-27 10:07:45 +0800

    sadf

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,22 @@ var DB = require('./../modules/db.js');
 
 //首页
 router.get('/', function (req, res, next) {
-  console.log(req.query);
+  // console.log(req.query);
   //查询条数
   DB.findCount('myData', 'articleData', {}, function (err, result) {
     console.log(result);
+  });
+
+  //测试promise
+  let p = new Promise(function (resolve, reject) {
+    //异步操作
+    setTimeout(function () {
+      console.log('promise执行完毕');
+      resolve('返回数据');
+    }, 2000);
+  });
+  p.then(data => {
+    console.log(data);
   });
 
   res.render('pages/index', { title: '纪路' });
